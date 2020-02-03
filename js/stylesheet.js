3fd3c6b790aafbb6c3c8efc7f583573d9f85c8e1
flaireclair <chateau.veau.gyuho@gmail.com>
2020-02-03 11:56:20 +0900

    fix

--- js/stylesheet.js
+++ js/stylesheet.js
@@ -1,15 +1,18 @@
 $(function() {
-        var $header = $('#top-head');
-        // Nav Fixed
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > 350) {
-                $header.addClass('fixed');
-            } else {
-                $header.removeClass('fixed');
-            }
-        });
-        // Nav Toggle Button
-        $('#nav-toggle').click(function(){
-            $header.toggleClass('open');
-        });
+    var $header = $('#top-head');
+
+    // Nav Fixed
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            $header.addClass('fixed');
+        } else {
+            $header.removeClass('fixed');
+        }
     });
+
+    // Nav Toggle Button
+    $('#nav-toggle').click(function(){
+        $header.toggleClass('open');
+    });
+
+});
