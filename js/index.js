184413d51ff13e4586161256b9233e2fe7c2ddba
flaireclair <h.kmntag6.gaburiasu@gmail.com>
2020-02-16 11:59:29 +0900

    サイドバーにHokkaido,Tohoku,Kantoを追加

--- js/index.js
+++ js/index.js
@@ -7,7 +7,18 @@ $(function () {
             $(this).removeClass('is-active');
         }
     });
+    $('.sub-sub-menu').on({
+        'mouseenter': function () {
+            $(this).addClass('is-active');
+        },
+        'mouseleave': function () {
+            $(this).removeClass('is-active');
+        }
+    });
 
+    $('.sub-sub-menu').on('transitionend', function() {
+        if('sub-menu')
+    });
 
     $('#nav-toggle,#overlay').on('click', function() {
         $('body').toggleClass('open');
